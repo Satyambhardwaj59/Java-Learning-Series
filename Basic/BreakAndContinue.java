0c37b6e83b205f0da9e8a21cf7f65bfb0a9d6134
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2026-03-24 22:32:15 +0530

    Learn about break, continue and label

---
+++ Basic/BreakAndContinue.java
@@ -0,0 +1,31 @@
+package Basics;
+
+public class BreakAndContinue {
+
+	public static void main(String[] args) {
+	
+//		for(int i = 0; i <=20; i +=2) {
+//			System.out.println(i);
+//			if(i >= 12) break;
+//		}
+		
+//		int i = 0;
+//		
+//		while(i <= 5) {
+//			System.out.println(i);
+//			i++;
+//			if(i >= 4) break;
+//		}
+		
+		
+		// Continue
+		
+		for(int i = 0; i <=20; i +=2) {
+			if(i == 12 || i == 8) continue;
+			System.out.println(i);
+		
+	}
+
+	}
+
+}

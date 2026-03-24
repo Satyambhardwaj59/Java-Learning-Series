e2c4cf503dea59cc9b36b55ba43b02a571802c32
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2026-03-24 22:30:59 +0530

    Learn about while and do while loop

---
+++ Basic/WhileLoop.java
@@ -0,0 +1,41 @@
+package Basics;
+import java.util.*;
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		
+//		int i = 0;
+//		
+//		while(i <= 5) {
+//			System.out.println(i);
+//			i++;
+//		}
+		
+		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		boolean hasLearnt = false;
+//		
+//		while(!hasLearnt) {
+//			System.out.println(" Still Lerning ");
+//			System.out.println(" Have you understood? ");
+//			hasLearnt = sc.nextBoolean();
+//		}
+//		
+//		System.out.println("Learning Completed!!!");
+		
+		
+		// do while loop
+		
+		int n = 8;
+		
+		do {
+			System.out.println(n);
+			n++;
+		}while(n <= 5);
+		
+		System.out.println("Out of loop");
+
+	}
+
+}

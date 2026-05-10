ef0b0b679c459d8508c01de52c8fa5350b64ca66
satyambhardwaj59 <satyambhardwaj59@gmail.com>
2026-05-10 22:41:56 +0530

    print number in spiral form

---
+++ Array/SpiralMatrix.java
@@ -0,0 +1,66 @@
+package Array;
+
+public class SpiralMatrix {
+	
+	public static void printSpiral(int matrix[][]) {
+		
+		int startRow = 0, startCol = 0, endRow = matrix.length - 1, endCol = matrix[0].length -1;
+		
+		while(startRow <= endRow && startCol <= endCol) {
+			
+			// top
+			for(int j = startCol; j <= endCol; j++) {
+				System.out.print(matrix[startRow][j] + " ");
+			}
+			
+			// right 
+			
+			for(int i = startRow + 1; i <= endRow; i++ ) {
+				System.out.print(matrix[i][endCol] + " ");
+			}
+			
+			// bottom 
+			
+			for(int j = endCol - 1; j >= startCol; j --) {
+				if(startRow == endRow) {
+					break;
+				}
+				System.out.print(matrix[endRow][j] + " ");
+			}
+			
+			// left 
+			
+			for(int i = endCol - 1; i >= startCol +1; i--) {
+				
+				if(startCol == endCol) {
+					break;
+				}
+				
+				System.out.print(matrix[i][startCol] + " ");
+			}
+			
+			startRow++;
+			endRow--;
+			startCol++;
+			endCol--;
+		}
+		
+		
+	}
+
+	public static void main(String[] args) {
+		
+		int matrix[][] = {
+				{1, 2, 3, 4 },
+				{5, 6, 7, 8},
+				{9, 10, 11, 12},
+				{13, 14, 15, 16}
+		};
+		
+		// output = 1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10
+		
+		printSpiral(matrix);
+
+	}
+
+}
